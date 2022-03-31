ba6a62f7f6aaf5f509fbcf9bad47848d1e1bdce4
Saiprava1234 <86531553+Saiprava1234@users.noreply.github.com>
2022-03-31 16:35:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(450,400);
+
+    canvas = createCanvas(450,400);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized!');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+function draw(){
+    background('#ffff00');
+}
